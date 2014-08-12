be528c2ddb76c80607f61393efa4229d229d342c
Enno Boland <g@s01.de>
2014-08-12 13:57:57 +0200

    add dummy classes

---
+++ src/main/java/de/qabel/core/DummyClass.java
@@ -0,0 +1,10 @@
+package de.qabel.core;
+
+public class DummyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/test/de/qabel/core/DummyClassTest.java
@@ -0,0 +1,14 @@
+package de.qabel.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyClassTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
