b4d3b33b87c7bf18e39760d9348f68b9cc6d59ab
L-Henke <henke@praemandatum.de>
2014-09-30 17:32:49 +0200

    Fixed CryptoUtils rsa sign fix again

--- src/main/java/de/qabel/core/crypto/CryptoUtils.java
+++ src/main/java/de/qabel/core/crypto/CryptoUtils.java
@@ -173,7 +173,7 @@ public class CryptoUtils {
 	 * @return Signature over SHA512 sum of message
 	 */
 	private byte[] rsaSign(byte[] message, QblPrimaryKeyPair qpkp) {
-		return rsaSign(message, qpkp);
+		return rsaSign(message, qpkp.getQblSignPrivateKey());
 	}
 
 	/**
