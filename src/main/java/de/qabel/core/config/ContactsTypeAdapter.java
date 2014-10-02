acc5a291dcaa9640bce80f78775b14f97a8d32ec
Fabian Träger <traeger@praemandatum.de>
2014-10-02 17:11:47 +0200

    new classes: * AccountsTypeAdapter * ContactsTypeAdapter * DropServersTypeAdapter * IdentitiesTypeAdapter * StorageServersTypeAdapter * StorageVolumesTypeAdapter

---
+++ src/main/java/de/qabel/core/config/ContactsTypeAdapter.java
@@ -0,0 +1,48 @@
+package de.qabel.core.config;
+
+import java.io.IOException;
+import java.util.Set;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class ContactsTypeAdapter extends TypeAdapter<Contacts> {
+
+	@Override
+	public void write(JsonWriter out, Contacts value) throws IOException {
+		out.beginArray();
+		Gson gson = new Gson();
+		Set<Contact> set = value.getContacts();
+		TypeAdapter<Contact> adapter = gson.getAdapter(Contact.class);
+		for(Contact contact : set) {
+			adapter.write(out, contact);
+		}
+		out.endArray();
+		return;
+	}
+
+	@Override
+	public Contacts read(JsonReader in) throws IOException {
+		if(in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+		
+		Gson gson = new Gson();
+		Contacts contacts = new Contacts();
+		TypeAdapter<Contact> adapter = gson.getAdapter(Contact.class);
+		Contact contact = null; 
+		
+		in.beginArray();
+		while(in.hasNext()) {
+			contact = adapter.read(in);
+			contacts.getContacts().add(contact);
+		}
+		in.endArray();
+		
+		return contacts;
+	}
+}
---
+++ src/main/java/de/qabel/core/config/StorageVolumesTypeAdapter.java
@@ -0,0 +1,48 @@
+package de.qabel.core.config;
+
+import java.io.IOException;
+import java.util.Set;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class StorageVolumesTypeAdapter extends TypeAdapter<StorageVolumes> {
+
+	@Override
+	public void write(JsonWriter out, StorageVolumes value) throws IOException {
+		out.beginArray();
+		Gson gson = new Gson();
+		Set<StorageVolume> set = value.getStorageVolume();
+		TypeAdapter<StorageVolume> adapter = gson.getAdapter(StorageVolume.class);
+		for(StorageVolume storageVolume : set) {
+			adapter.write(out, storageVolume);
+		}
+		out.endArray();
+		return;
+	}
+
+	@Override
+	public StorageVolumes read(JsonReader in) throws IOException {
+		if(in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+		
+		Gson gson = new Gson();
+		StorageVolumes storageVolumes = new StorageVolumes();
+		TypeAdapter<StorageVolume> adapter = gson.getAdapter(StorageVolume.class);
+		StorageVolume storageVolume = null; 
+		
+		in.beginArray();
+		while(in.hasNext()) {
+			storageVolume = adapter.read(in);
+			storageVolumes.getStorageVolume().add(storageVolume);
+		}
+		in.endArray();
+		
+		return storageVolumes;
+	}
+}
