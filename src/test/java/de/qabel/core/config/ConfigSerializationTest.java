a0be290e1b284b005b2f513d8238f47ca6f5d2c8
Fabian Träger <traeger@praemandatum.de>
2014-10-02 18:22:46 +0200

    added test for config serialization

---
+++ src/test/java/de/qabel/core/config/ConfigSerializationTest.java
@@ -0,0 +1,86 @@
+package de.qabel.core.config;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ConfigSerializationTest {	
+	
+	@Test
+	public void settingsTest() {
+		SyncedSettings syncedSettings = new SyncedSettings();
+		
+		//generate "accounts" array
+		syncedSettings.setAccounts(new Accounts());
+		//generate and add an "accounts" entry
+		Account account = new Account("provider", "user", "auth");
+		syncedSettings.getAccounts().getAccount().add(account);
+		
+		//generate "drop_servers" array
+		syncedSettings.setDropServers(new DropServers());
+		//generate and add an "drop_servers" entry
+		try {
+			DropServer dropServer = new DropServer(new URL("https://drop.qabel.de/0123456789012345678901234567890123456789123"),"auth", true);
+			syncedSettings.getDropServers().getDropServer().add(dropServer);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		//generate "identities" array
+		syncedSettings.setIdentities(new Identities());
+		//generate and add an "identities" entry
+		try {
+			Identity identity = new Identity("alias", new URL("https://inbox.qabel.de"));
+			syncedSettings.getIdentities().getIdentity().add(identity);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		//generate "storage_servers" array
+		syncedSettings.setStorageServers(new StorageServers());
+		//generate and add a "storage_servers" entry
+		try {
+			StorageServer storageServer = new StorageServer(new URL("https://storage.qabel.de"), "auth");
+			syncedSettings.getStorageServers().getStorageServer().add(storageServer);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		//generate "storage_volumes" array
+		syncedSettings.setStorageVolumes(new StorageVolumes());
+		//generate and add a "storage_volumes" entry
+		syncedSettings.getStorageVolumes().getStorageVolume().add(new StorageVolume("publicIdentifier", "token", "revokeToken"));
+		syncedSettings.setSyncedModuleSettings(new SyncedModuleSettings());
+		syncedSettings.getSyncedModuleSettings().getSyncedSettings();
+		
+		//
+		LocalSettings localSettings = new LocalSettings(null, 10, new Date(System.currentTimeMillis()));		
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Accounts.class, new AccountsTypeAdapter());
+		builder.registerTypeAdapter(DropServers.class, new DropServersTypeAdapter());
+		builder.registerTypeAdapter(Identities.class, new IdentitiesTypeAdapter());
+		builder.registerTypeAdapter(StorageServers.class, new StorageServersTypeAdapter());
+		builder.registerTypeAdapter(StorageVolumes.class, new StorageVolumesTypeAdapter());
+		builder.setDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Gson gson = builder.create();
+		LocalSettings deserializedLocalSettings = gson.fromJson(gson.toJson(localSettings), LocalSettings.class);
+		SyncedSettings deserializedSyncedSettings = gson.fromJson(gson.toJson(syncedSettings), SyncedSettings.class);
+		System.out.println(gson.toJson(syncedSettings));
+		System.out.println(gson.toJson(deserializedSyncedSettings));
+		System.out.println("Local settings: " + gson.toJson(localSettings));
+		System.out.println("Deserialized local settings: " + gson.toJson(deserializedLocalSettings));
+		
+		assertEquals(deserializedSyncedSettings, syncedSettings);
+		assertEquals(deserializedLocalSettings, localSettings);
+	}
+}
