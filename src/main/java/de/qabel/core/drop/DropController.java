ce60210b20c36823d22dbc8b87b973778b168352
L-Henke <henke@praemandatum.de>
2014-10-15 15:29:40 +0200

    Removed onDropEvent interface from DropListener

--- src/main/java/de/qabel/core/drop/DropController.java
+++ src/main/java/de/qabel/core/drop/DropController.java
@@ -15,46 +15,36 @@ public class DropController {
 	 *           dropController        &gt;       dropListener
 	 * </pre>
 	 */
-	private Map<Class<? extends ModelObject>, ArrayList<DropListener>> dropListeners;
-	private Map<DropListener, ArrayList<BlockingQueue<DropMessage<ModelObject>>>> blockingQueues;
+	private Map<Class<? extends ModelObject>, ArrayList<BlockingQueue<DropMessage<ModelObject>>>> mapBlockingQueues;
 
 	public DropController() {
-		this.dropListeners = new HashMap<Class<? extends ModelObject>, ArrayList<DropListener>>();
-		this.blockingQueues = new HashMap<DropListener, ArrayList<BlockingQueue<DropMessage<ModelObject>>>>();
+		this.mapBlockingQueues = new HashMap<Class<? extends ModelObject>, ArrayList<BlockingQueue<DropMessage<ModelObject>>>>();
 	}
 
-	public BlockingQueue<DropMessage<ModelObject>> register(ModelObject modelObject, DropListener dropListener) {
-		ArrayList<DropListener> ar = dropListeners.get(modelObject.getClass());
-		if (ar == null) {
-			ar = new ArrayList<DropListener>();
-			dropListeners.put(modelObject.getClass(), ar);
-		}
-		ar.add(dropListener);
-		
-		ArrayList<BlockingQueue<DropMessage<ModelObject>>> listBlockingQueue = blockingQueues
+	public BlockingQueue<DropMessage<ModelObject>> register(
+			ModelObject modelObject) {
+
+		ArrayList<BlockingQueue<DropMessage<ModelObject>>> alBlockingQueue = mapBlockingQueues
 				.get(modelObject.getClass());
-		if (listBlockingQueue == null){
-			listBlockingQueue = new ArrayList<BlockingQueue<DropMessage<ModelObject>>>();
-			blockingQueues.put(dropListener, listBlockingQueue);
+		if (alBlockingQueue == null) {
+			alBlockingQueue = new ArrayList<BlockingQueue<DropMessage<ModelObject>>>();
+			mapBlockingQueues.put(modelObject.getClass(), alBlockingQueue);
 		}
-		
+
 		BlockingQueue<DropMessage<ModelObject>> bq = new LinkedBlockingQueue<DropMessage<ModelObject>>();
-		listBlockingQueue.add(bq);
+		alBlockingQueue.add(bq);
 		return bq;
 	}
 
 	public void handleDrop(DropMessage<? extends ModelObject> dm) {
-		for (DropListener dl : dropListeners.get(dm.getModelObject())) {
-			dl.onDropEvent((DropMessage<ModelObject>) dm);
-			
-			for (BlockingQueue<DropMessage<ModelObject>> bq : blockingQueues.get(dl)){
-				try {
-					bq.put((DropMessage<ModelObject>) dm);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}			
+		for (BlockingQueue<DropMessage<ModelObject>> bq : mapBlockingQueues
+				.get(dm.getModelObject())) {
+			try {
+				bq.put((DropMessage<ModelObject>) dm);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/test/java/de/qabel/core/drop/DropListenerTest.java
+++ src/test/java/de/qabel/core/drop/DropListenerTest.java
@@ -1,19 +1,14 @@
 package de.qabel.core.drop;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Date;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 public class DropListenerTest {
-
-	final static int expectedTestMO1HandlerCalls = 2;
-	final static int expectedTestMO2HandlerCalls = 0;
-
-	static int testMO1HandlerCalled = 0;
-	static int testMO2HandlerCalled = 0;
 
 	static public class TestMO1 extends ModelObject {
 		public String content;
@@ -23,45 +18,8 @@ public class DropListenerTest {
 		public String content;
 	}
 
-	static class DropListener1 implements DropListener {
-		@Override
-		public void onDropEvent(DropMessage<ModelObject> dropMessage) {
-			assertEquals(1, dropMessage.getVersion());
-			assertEquals("foo", dropMessage.getSender());
-			assertEquals("bar", dropMessage.getAcknowledgeID());
-			assertEquals(1412687357, dropMessage.getTime());
-			assertEquals("payload data",
-					((TestMO1) dropMessage.getData()).content);
-			testMO1HandlerCalled++;
-		}
-	}
-
-	static class DropListener2 implements DropListener {
-		@Override
-		public void onDropEvent(DropMessage<ModelObject> dropMessage) {
-			assertEquals(1, dropMessage.getVersion());
-			assertEquals("foo", dropMessage.getSender());
-			assertEquals("bar", dropMessage.getAcknowledgeID());
-			assertEquals(1412687357, dropMessage.getTime());
-			assertEquals("payload data",
-					((TestMO1) dropMessage.getData()).content);
-			testMO1HandlerCalled++;
-		}
-	}
-
-	static class DropListener3 implements DropListener {
-		@Override
-		public void onDropEvent(DropMessage<ModelObject> dropMessage) {
-			testMO2HandlerCalled++;
-		}
-	}
-
 	@Test
 	public void dropListenerTest() throws InterruptedException {
-
-		DropListener dl1 = new DropListener1();
-		DropListener dl2 = new DropListener2();
-		DropListener dl3 = new DropListener3();
 
 		TestMO1 mo1 = new TestMO1();
 		mo1.content = "payload data";
@@ -70,9 +28,10 @@ public class DropListenerTest {
 		mo1.content = "payload data";
 
 		DropController dc = new DropController();
-		BlockingQueue<DropMessage<ModelObject>> bq1 = dc.register(mo1, dl1);
-		BlockingQueue<DropMessage<ModelObject>> bq2 = dc.register(mo1, dl2);
-
+		BlockingQueue<DropMessage<ModelObject>> bq1 = dc.register(mo1);
+		BlockingQueue<DropMessage<ModelObject>> bq2 = dc.register(mo1);
+		BlockingQueue<DropMessage<ModelObject>> bq3 = dc.register(mo2);
+		
 		DropMessage<TestMO1> dm = new DropMessage<TestMO1>();
 		Date date = new Date(1412687357);
 
@@ -83,13 +42,10 @@ public class DropListenerTest {
 		dm.setVersion(1);
 		dm.setModelObject(TestMO1.class);
 
-		// DropListener1 and DropListener2 should be called for this DropMessage
-		// type while DropListener3 should remain uncalled.
 		dc.handleDrop(dm);
-		assertEquals(expectedTestMO1HandlerCalls, testMO1HandlerCalled);
-		assertEquals(expectedTestMO2HandlerCalls, testMO2HandlerCalled);
-		
+	
 		assertEquals(dm, bq1.take());
 		assertEquals(dm, bq2.take());
+		assertNull(bq3.poll(1L, TimeUnit.MILLISECONDS));
 	}
 }
