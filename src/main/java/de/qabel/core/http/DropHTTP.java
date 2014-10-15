1aec183df02b83d5fb63b36eef091a644fc2bf6b
Nicola Hauke <jasinai@jasinai.de>
2014-10-15 16:21:13 +0200

    ReceiveMessages returns a Collection<String>

--- src/main/java/de/qabel/core/http/DropHTTP.java
+++ src/main/java/de/qabel/core/http/DropHTTP.java
@@ -1,5 +1,9 @@
 package de.qabel.core.http;
 
+import org.apache.james.mime4j.MimeException;
+import org.apache.james.mime4j.stream.EntityState;
+import org.apache.james.mime4j.stream.MimeTokenStream;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,6 +12,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class DropHTTP {
 
@@ -38,29 +44,38 @@ public class DropHTTP {
 		return responseCode;
 	}
 
-	public String receiveMessages(URL url) {
+	public Collection<String> receiveMessages(URL url) {
 		return this.receiveMessages(url, 0);
 	}
 
-	public String receiveMessages(URL url, long sinceDate) {
+	public Collection<String> receiveMessages(URL url, long sinceDate) {
 		int responseCode = 0;
 		HttpURLConnection conn = (HttpURLConnection) this.setupConnection(url);
 		conn.setIfModifiedSince(sinceDate);
-		String result = "";
+		Collection<String> messages = new ArrayList<String>();
 		try {
 			conn.setRequestMethod("GET");
 			responseCode = conn.getResponseCode();
 			if (responseCode == 200) {
-				InputStream response = conn.getInputStream();
-				result = this.convertStreamToString(response);
+				InputStream inputstream = conn.getInputStream();
+				MimeTokenStream stream = new MimeTokenStream();
+				stream.parse(inputstream);
+				for (EntityState state = stream.getState();
+					 state != EntityState.T_END_OF_STREAM;
+					 state = stream.next()) {
+					if (state == EntityState.T_BODY) {
+						messages.add(this.convertStreamToString(stream.getInputStream()));
+					}
+				}
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MimeException e) {
 			e.printStackTrace();
 		} finally {
 			conn.disconnect();
 		}
-		return result;
+		return messages;
 	}
 
 	public int head(URL url) {
