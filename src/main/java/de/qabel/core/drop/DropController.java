48cfab9f6ba0bc76b10734600f79690ad5c2f083
Enno Boland <g@s01.de>
2014-10-17 02:00:53 +0200

    DropController: reintroducing the callback class

--- src/main/java/de/qabel/core/drop/DropController.java
+++ src/main/java/de/qabel/core/drop/DropController.java
@@ -1,24 +1,17 @@
 package de.qabel.core.drop;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Set;
 
 public class DropController {
 
-	/**
-	 * <pre>
-	 *           0..*     0..*
-	 * DropController ------------------------> DropListener
-	 *           dropController        &gt;       dropListener
-	 * </pre>
-	 */
-	private Map<Class<? extends ModelObject>, ArrayList<BlockingQueue<DropMessage<ModelObject>>>> mapBlockingQueues;
+	Map<Class<? extends ModelObject>, Set<DropCallback<? extends ModelObject>>> mCallbacks;
 
 	public DropController() {
-		this.mapBlockingQueues = new HashMap<Class<? extends ModelObject>, ArrayList<BlockingQueue<DropMessage<ModelObject>>>>();
+		mCallbacks = new HashMap<Class<? extends ModelObject>, Set<DropCallback<? extends ModelObject>>>();
 	}
 
 	/**
@@ -29,19 +22,15 @@ public class DropController {
 	 * @return a queue in which the DropController will put received
 	 *         DropMessages with the selected ModelObject
 	 */
-	public BlockingQueue<DropMessage<ModelObject>> register(
-			ModelObject modelObject) {
-
-		ArrayList<BlockingQueue<DropMessage<ModelObject>>> alBlockingQueue = mapBlockingQueues
-				.get(modelObject.getClass());
-		if (alBlockingQueue == null) {
-			alBlockingQueue = new ArrayList<BlockingQueue<DropMessage<ModelObject>>>();
-			mapBlockingQueues.put(modelObject.getClass(), alBlockingQueue);
+	public <T extends ModelObject> void register(Class<T> type,
+			DropCallback<T> callback) {
+		Set<DropCallback<? extends ModelObject>> typeCallbacks = mCallbacks
+				.get(type);
+		if (typeCallbacks == null) {
+			typeCallbacks = new HashSet<DropCallback<? extends ModelObject>>();
+			mCallbacks.put(type, typeCallbacks);
 		}
-
-		BlockingQueue<DropMessage<ModelObject>> bq = new LinkedBlockingQueue<DropMessage<ModelObject>>();
-		alBlockingQueue.add(bq);
-		return bq;
+		typeCallbacks.add(callback);
 	}
 
 	/**
@@ -52,11 +41,17 @@ public class DropController {
 	 *            DropMessage which should be handled
 	 */
 	public void handleDrop(DropMessage<? extends ModelObject> dm) {
-		for (BlockingQueue<DropMessage<ModelObject>> bq : mapBlockingQueues
-				.get(dm.getModelObject())) {
+		Class<? extends ModelObject> cls = dm.getModelObject();
+		Set<DropCallback<? extends ModelObject>> typeCallbacks = mCallbacks
+				.get(cls);
+
+		for (DropCallback<? extends ModelObject> callback : typeCallbacks) {
+			Method m;
 			try {
-				bq.put((DropMessage<ModelObject>) dm);
-			} catch (InterruptedException e) {
+				m = callback.getClass().getMethod("onDropMessage",
+						ModelObject.class);
+				m.invoke(callback, dm.getData());
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
