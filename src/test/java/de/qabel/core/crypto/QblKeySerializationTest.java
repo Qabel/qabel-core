798f9ce1e298179c4ddbb2c6da42599c1e4a016c
Fabian Träger <traeger@praemandatum.de>
2014-10-23 12:37:45 +0200

    added test for QblPrimaryPublicKey serialization

--- src/test/java/de/qabel/core/crypto/QblKeySerializationTest.java
+++ src/test/java/de/qabel/core/crypto/QblKeySerializationTest.java
@@ -22,4 +22,21 @@ public class QblKeySerializationTest {
 		
 		assertEquals(qpkp, deserializedQpkp);
 	}
+	
+	@Test
+	public void qblPrimaryPublicKeyTest() {
+		QblKeyFactory kf = QblKeyFactory.getInstance();
+		QblPrimaryKeyPair qpkp = kf.generateQblPrimaryKeyPair();
+		QblPrimaryPublicKey qppk = qpkp.getQblPrimaryPublicKey();
+		qppk.attachEncPublicKey(qpkp.getQblEncPublicKey());
+		qppk.attachSignPublicKey(qpkp.getQblSignPublicKey());
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(QblPrimaryPublicKey.class, new QblPrimaryPublicKeyTypeAdapter());
+		Gson gson = builder.setPrettyPrinting().create();
+		System.out.println("Serialized key: " + gson.toJson(qppk));
+		QblPrimaryPublicKey deserializedQppk = gson.fromJson(gson.toJson(qppk), QblPrimaryPublicKey.class);
+		System.out.println("Deserialized key: " + gson.toJson(deserializedQppk));
+		
+		assertEquals(qppk, deserializedQppk);
+	}
 }
