030df1dd546b1fe2b2aa27d25f50677032afbf9c
Alexander Bothe <info@alexanderbothe.com>
2014-10-28 10:23:57 +0100

    Init DropMessage's acknowledgeID with "0" instead of an empty string, as this is stated in the docs #156

--- src/main/java/de/qabel/core/drop/DropMessage.java
+++ src/main/java/de/qabel/core/drop/DropMessage.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class DropMessage<T extends ModelObject>{
     private int version = 1;
     private long time = 1L;
-    private String acknowledgeID = "";
+    private String acknowledgeID = "0";
     private String sender = "";
     private Class<T> modelObject;
     private T data;
