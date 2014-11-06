0a9db0bc6dba8f5c0ee2c23f01d00a23169847ad
Fabian Träger <traeger@praemandatum.de>
2014-11-06 17:08:29 +0100

    In class Identities: * changed collection type from Set to Map in order to enable retrieving identity by key identifier * adapted change of collection type in add() method * added getter getIdentityByKeyIdentifier() * added javadoc for new and modified methods

--- src/main/java/de/qabel/core/config/Identities.java
+++ src/main/java/de/qabel/core/config/Identities.java
@@ -1,6 +1,8 @@
 package de.qabel.core.config;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -15,15 +17,41 @@ public class Identities {
 	 * Identities ------------------------- Identity
 	 *           identities        &gt;       identity
 	 * </pre>
+	 * 
+	 * Map of pairs <KeyIdentifier, Identity>. The key identifier consists of the right most 64 bit of the Identity's public fingerprint. 
 	 */
-	private final Set<Identity> identities = new HashSet<Identity>();
+	private final Map<String, Identity> identities = new HashMap<String, Identity>();
 	
+	/**
+	 * Get an unmodifiable list of stored instances of Identity
+	 * @return unmodifiable set of Identity
+	 */
 	public Set<Identity> getIdentities() {
-		return Collections.unmodifiableSet(this.identities);
+		return Collections.unmodifiableSet(new HashSet<Identity>(this.identities.values()));
 	}
 	
+	/**
+	 * Add an instance of Identity to the identities list.
+	 * @param identity
+	 * @return true if identity has been added - false if this identity is a duplicate
+	 */
 	public boolean add(Identity identity) {
-		return this.identities.add(identity);
+		if (this.identities.containsKey(identity.getKeyIdentifier())) {
+			return false;
+		}
+		else {
+			this.identities.put(identity.getKeyIdentifier(), identity);
+			return true;
+		}
+	}
+
+	/**
+	 * Get Identity by key identifier (right most 64 bit of the identity's public fingerprint)
+	 * @param keyIdentifier
+	 * @return identity to which the key identifier is mapped or null if there is no mapping for this key identifier
+	 */
+	public Identity getIdentityByKeyIdentifier(String keyIdentifier) {
+		return this.identities.get(keyIdentifier);
 	}
 
 	@Override
