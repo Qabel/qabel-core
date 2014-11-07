19a234683a52b71665201f24447d0e3276b5eb5b
Fabian Träger <traeger@praemandatum.de>
2014-11-07 17:03:14 +0100

    added javadoc in class DropServer

--- src/main/java/de/qabel/core/config/DropServer.java
+++ src/main/java/de/qabel/core/config/DropServer.java
@@ -2,21 +2,40 @@ package de.qabel.core.config;
 
 import java.net.URL;
 
+/** 
+ * https://github.com/Qabel/qabel-doc/wiki/Qabel-Client-Configuration#drop-server
+ */
 public class DropServer {
 	private int id;
 	private int updated;
 	private int created;
 	private int deleted;
+	/**
+	 * URL to the drop service without the drop id
+	 */
 	private URL url;
+	/**
+	 * Authentication for the drop server (Credential for optional, additional access regulation)
+	 */	
 	private String auth;
+	/**
+	 * Status flag of the drop server
+	 */
 	private boolean active;
-	
+	/**
+	 * Creates an instance of DropServer
+	 * @param url
+	 * @param auth
+	 * @param active
+	 */
 	public DropServer(URL url, String auth, boolean active) {
 		this.setUrl(url);
 		this.setAuth(auth);
 		this.setActive(active);
 	}
-	
+	/**
+	 * Creates an instance of DropServer
+	 */
 	public DropServer() {
 		
 	}
@@ -56,29 +75,50 @@ public class DropServer {
 		return this.deleted;
 	}
 
-
+	/**
+	 * Sets the url of the drop server
+	 * @param value
+	 */
 	public void setUrl(URL value) {
 		this.url = value;
 	}
 
+	/**
+	 * Returns the url of the drop server
+	 * @return URL
+	 */
 	public URL getUrl() {
 		return this.url;
 	}
 
-
+	/**
+	 * Sets the authentication of the drop server
+	 * @param value
+	 */
 	public void setAuth(String value) {
 		this.auth = value;
 	}
 
+	/**
+	 * Returns the authentication of the drop server
+	 * @return authentication
+	 */
 	public String getAuth() {
 		return this.auth;
 	}
 
-
+	/**
+	 * Sets the status flag of the drop server
+	 * @param value
+	 */
 	public void setActive(boolean value) {
 		this.active = value;
 	}
 
+	/**
+	 * Returns the status flag of the drop server
+	 * @return boolean
+	 */
 	public boolean isActive() {
 		return this.active;
 	}
