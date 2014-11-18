77a44e4992a564a6a6433db78306ff8bba239273
Michael Ott <michael@king-coder.de>
2014-11-18 12:33:47 +0100

    Add new classes DropResultContact and DropResult

---
+++ src/main/java/de/qabel/core/drop/DropResult.java
@@ -0,0 +1,74 @@
+package de.qabel.core.drop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Class DropResult: Save the return value of every contact and the overview
+ * whether everything is okay
+ */
+public class DropResult {
+	private boolean checked = false;
+	private boolean success = true;
+	private List<DropResultContact> results;
+
+	public DropResult() {
+		this.results = new ArrayList<DropResultContact>();
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param success
+	 *            State of the shipping
+	 * @param results
+	 *            List of all results
+	 */
+	public DropResult(boolean success, List<DropResultContact> results) {
+		this.success = success;
+		this.results = results;
+	}
+
+	void addContactResult(DropResultContact result) {
+		this.results.add(result);
+		
+		this.checked = true;
+		
+		if (result.isSuccess() == false) {
+			this.success = false;
+		}
+	}
+	/**
+	 * Return the overview state of the complete shipping
+	 * 
+	 * @return State of the shipping
+	 */
+	public boolean isSuccess() {
+		if (this.checked == false) {
+			return (false);
+		}
+
+		return (this.success);
+	}
+
+	/**
+	 * Return the list of all results of the contacts
+	 * 
+	 * @return List of all results
+	 */
+	public List<DropResultContact> getList() {
+		return (Collections.unmodifiableList(this.results));
+	}
+
+	/**
+	 * Add one result
+	 * 
+	 * @param pair
+	 *            Object of DropResultContact
+	 */
+	public void addContact(DropResultContact result) {
+		this.results.add(result);
+	}
+
+}
---
+++ src/main/java/de/qabel/core/drop/DropResultContact.java
@@ -0,0 +1,73 @@
+package de.qabel.core.drop;
+
+import java.util.Arrays;
+
+import de.qabel.core.config.Contact;
+
+/**
+ * Class DropResultPair: Save the return value of a contact
+ *
+ */
+public class DropResultContact {
+	private Contact contact;
+	private int[] errorCode = new int[0];
+	private boolean success = false;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param contact
+	 *            Contact object
+	 * @param success
+	 *            Return state
+	 */
+	DropResultContact(Contact contact) {
+		this.contact = contact;
+		
+		return;
+	}
+
+	boolean addErrorCode(int errorCode) {
+		if (errorCode == 200) {
+			this.success = true;
+		}
+		
+	    this.errorCode  = Arrays.copyOf(this.errorCode, this.errorCode.length + 1);
+		this.errorCode[this.errorCode.length - 1] = errorCode;
+		
+		return (this.success);
+	}
+
+	/**
+	 * Get contact
+	 * 
+	 * @return Contact object
+	 */
+	public Contact getContact() {
+		return this.contact;
+	}
+
+	/**
+	 * Return error code
+	 * 
+	 * @return Error code
+	 */
+	public int[] getErrorCode() {
+		return (this.errorCode);
+	}
+
+	/**
+	 * Get the return state of the shipping
+	 * 
+	 * @return Return state
+	 */
+	public boolean isSuccess() {
+		return (this.success);
+	}
+
+	private int[] addElement(int[] array, int element) {
+	    array  = Arrays.copyOf(array, array.length + 1);
+	    array[array.length - 1] = element;
+	    return array;
+	}
+}
