76b6825dc5203ca9abdc717b29e9d94804ace406
Fabian Träger <traeger@praemandatum.de>
2014-12-02 13:39:57 +0100

    changed equals() method test for class Account to use MeanBean library

--- src/test/java/de/qabel/core/config/ConfigEqualsTest.java
+++ src/test/java/de/qabel/core/config/ConfigEqualsTest.java
@@ -1,31 +1,13 @@
 package de.qabel.core.config;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import org.meanbean.test.EqualsMethodTester;
 
 public class ConfigEqualsTest {
 
 	@Test
-	public void AccountEqualsTest() {
-		Account a = new Account("a", "s", "d");
-		Account b = new Account("d", "e", "f");
-		Account c = new Account("a", "s", "d");
-
-		assertNotEquals(a, b);
-		assertNotEquals(b, c);
-		assertEquals(a, c);
-
-		Accounts as = new Accounts();
-		as.add(a);
-		Accounts bs = new Accounts();
-		bs.add(b);
-		Accounts cs = new Accounts();
-		cs.add(c);
-
-		assertNotEquals(as, bs);
-		assertNotEquals(bs, cs);
-		assertEquals(as, cs);
+	public void accountEqualsTest() {
+		EqualsMethodTester tester = new EqualsMethodTester();
+		tester.testEqualsMethod(new AccountEquivalentTestFactory());
 	}
 }
