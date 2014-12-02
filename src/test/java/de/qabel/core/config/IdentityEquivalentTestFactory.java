f8bd057e54229f8aa91fa6fb535cc5c1e8a13529
Fabian Träger <traeger@praemandatum.de>
2014-12-02 13:57:35 +0100

    added Identity equivalent factory for testing purposes

---
+++ src/test/java/de/qabel/core/config/IdentityEquivalentTestFactory.java
@@ -0,0 +1,34 @@
+package de.qabel.core.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.meanbean.lang.EquivalentFactory;
+
+import de.qabel.core.crypto.QblKeyFactory;
+import de.qabel.core.crypto.QblPrimaryKeyPair;
+import de.qabel.core.drop.DropURL;
+
+/**
+ * IdentityEquivalentTestFactory
+ * Creates logically equivalent instances of class Identity
+ * Attention: For testing purposes only
+ */
+class IdentityEquivalentTestFactory implements EquivalentFactory<Identity> {
+	QblPrimaryKeyPair qpkp;
+	List<DropURL> dropList;
+
+	IdentityEquivalentTestFactory() {
+		QblKeyFactory kf = QblKeyFactory.getInstance();
+		qpkp = kf.generateQblPrimaryKeyPair();
+
+		DropUrlListTestFactory dropListFactory = new DropUrlListTestFactory();
+		dropList = new ArrayList<DropURL>(dropListFactory.create());
+	}
+
+	@Override
+	public Identity create() {
+		Identity identity = new Identity("alias", dropList, qpkp);
+		return identity;
+	}
+}
