22c53181cef0e3e95bba56352e0286d6d50d87f4
Fabian Träger <traeger@praemandatum.de>
2014-12-04 16:03:47 +0100

    added missing javadoc to class AccountTestFactory

--- src/test/java/de/qabel/core/config/AccountTestFactory.java
+++ src/test/java/de/qabel/core/config/AccountTestFactory.java
@@ -2,6 +2,11 @@ package de.qabel.core.config;
 
 import org.meanbean.lang.Factory;
 
+/**
+ * AccountTestFactory
+ * Creates distinct instances of class Account
+ * Attention: For testing purposes only!
+ */
 class AccountTestFactory implements Factory<Account>{
 	int i = 0;
 
