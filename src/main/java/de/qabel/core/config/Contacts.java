d365296a6cd9c11c5aa9077ceeefd8f8cc3c8081
Christian Burkert <burkert@praemandatum.de>
2015-01-29 18:39:20 +0100

    Adapted Contacts to EnityMap

--- src/main/java/de/qabel/core/config/Contacts.java
+++ src/main/java/de/qabel/core/config/Contacts.java
@@ -1,71 +1,16 @@
 package de.qabel.core.config;
 
-import java.util.*;
+import java.util.Set;
 
 /** 
  * https://github.com/Qabel/qabel-doc/wiki/Qabel-Client-Contact-Drop-Messages#contacts
  */
-public class Contacts {
-	
-    	/**
-	 * <pre>
-	 *           1     0..*
-	 * Contacts ------------------------- Contact
-	 *           contacts        &gt;       contacts
-	 * </pre>
-	 */
-	private final Set<Contact> contacts = new HashSet<Contact>();
-
-	/**
+public class Contacts extends EntityMap<Contact> {
+    /**
 	 * Returns unmodifiable set of contained contacts
 	 * @return Set<Contact>
 	 */
 	public Set<Contact> getContacts() {
-		return Collections.unmodifiableSet(this.contacts);
+		return this.getEntities();
 	}
-	
-	/**
-	 * Adds a contact
-	 * @param contact
-	 * @return true if contact is added, false if contact is already in list
-	 */
-	public boolean add(Contact contact) {
-		return this.contacts.add(contact);
-	}
-
-	/**
-	 * Removes contact from list of contacts
-	 * @param contact
-	 * @return true if contact was contained in list, false if not
-	 */
-	public boolean remove(Contact contact) {
-		return this.contacts.remove(contact);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((contacts == null) ? 0 : contacts.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contacts other = (Contacts) obj;
-		if (contacts == null) {
-			if (other.contacts != null)
-				return false;
-		} else if (!contacts.equals(other.contacts))
-			return false;
-		return true;
-	}
-	
 }
