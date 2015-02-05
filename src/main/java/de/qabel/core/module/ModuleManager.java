c0aa35ceb652b48c93336a3a4a5b5cad4695ba32
Jan S <schreiber@praemandatum.de>
2015-02-05 15:32:35 +0100

    Continued with module package.
    Only module files in this commit.

--- src/main/java/de/qabel/core/module/ModuleManager.java
+++ src/main/java/de/qabel/core/module/ModuleManager.java
@@ -28,13 +28,6 @@ public class ModuleManager {
 
 	public final static ClassLoader LOADER = new ClassLoader();
 
-	/**
-	 * <pre>
-	 *           0..*     0..*
-	 * ModuleManager ------------------------- StorageConnection
-	 *           moduleManager        &lt;       storageConnection
-	 * </pre>
-	 */
 	private Set<StorageConnection> storageConnection;
 
 	public Set<StorageConnection> getStorageConnection() {
@@ -53,13 +46,6 @@ public class ModuleManager {
 		return this.settings;
 	}
 
-	/**
-	 * <pre>
-	 *           1..1     0..*
-	 * ModuleManager ------------------------- Module
-	 *           moduleManager        &gt;       modules
-	 * </pre>
-	 */
 	private Set<Module> modules;
 
 	Thread dropReceiverThread = new Thread() {
@@ -77,7 +63,7 @@ public class ModuleManager {
 
 	/**
 	 * Starts a given Module by its class
-	 * @param module
+	 * @param module Module to start.
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
 	 */
