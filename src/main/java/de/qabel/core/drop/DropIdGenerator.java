9e010fb8eedfe7ba22ed21d4ac84210258a40831
Christian Burkert <burkert@praemandatum.de>
2015-02-10 15:57:00 +0100

    Calculate length of base64 encoded id instead of hardcoding

--- src/main/java/de/qabel/core/drop/DropIdGenerator.java
+++ src/main/java/de/qabel/core/drop/DropIdGenerator.java
@@ -5,8 +5,15 @@ import java.util.Arrays;
 import org.bouncycastle.util.encoders.UrlBase64;
 
 public abstract class DropIdGenerator {
-	public static final int DROP_ID_LENGTH = 43;
+	/**
+	 * Length of the drop id in base64 encoding.
+	 */
 	public static final int DROP_ID_LENGTH_BYTE = 32;
+	/**
+	 * Length of the base64url encoded id.
+	 * The terminating padding character is not part of the id. 
+	 */
+	public static final int DROP_ID_LENGTH = 4 * ((DROP_ID_LENGTH_BYTE + 2) / 3) - 1;
 
 	public static DropIdGenerator getDefaultDropIdGenerator() {
 		return new AdjustableDropIdGenerator();
