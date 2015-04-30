85fe3ee02998c627b5193cb1b10ae6c4607a57d0
Christian Burkert <burkert@praemandatum.de>
2015-05-01 00:06:20 +0200

    Fixed module settings test

--- src/test/java/de/qabel/core/config/ModuleSettingsTest.java
+++ src/test/java/de/qabel/core/config/ModuleSettingsTest.java
@@ -9,40 +9,40 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
 
 public class ModuleSettingsTest {
-
-	private static class FooModuleSettings extends AbstractModuleSettings {
-		int answer;
-		
-		public FooModuleSettings(int answer) {
-			this.answer = answer;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			FooModuleSettings other = (FooModuleSettings) obj;
-			if (answer != other.answer) {
-				return false;
-			}
-			return true;
-		}
-	}
-	
 	@Test
 	public void serializeModuleSettings() throws IOException {
-		FooModuleSettings fooSettings = new FooModuleSettings(42);
-		TypeAdapter<? super FooModuleSettings> adapter = new GsonBuilder()
-			.registerTypeAdapter(AbstractModuleSettings.class, new AbstractModuleSettingsTypeAdapter())
-			.create().getAdapter(FooModuleSettings.class);
+		SyncedModuleSettings fooSettings = new FooModuleSettings(42);
+		TypeAdapter<SyncedModuleSettings> adapter = new GsonBuilder()
+			.registerTypeAdapter(SyncedModuleSettings.class, new SyncedModuleSettingsTypeAdapter())
+			.create().getAdapter(SyncedModuleSettings.class);
 		String json = adapter.toJson(fooSettings);
+		System.out.println(json);
 		Assert.assertEquals(fooSettings, adapter.fromJson(json));
+	}
+}
+
+class FooModuleSettings extends SyncedModuleSettings {
+	int answer;
+	
+	public FooModuleSettings(int answer) {
+		this.answer = answer;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		FooModuleSettings other = (FooModuleSettings) obj;
+		if (answer != other.answer) {
+			return false;
+		}
+		return true;
 	}
 }
