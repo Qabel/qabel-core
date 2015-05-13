7eabd5f0215f468f3e77d16bcad4f46c3bd18b76
L-Henke <henke@praemandatum.de>
2015-05-13 11:57:41 +0200

    Let ContactActor emit EVENT_CONTACT_ADDED and EVENT_CONTACT_REMOVED.

---
+++ src/main/java/de/qabel/core/EventNameConstants.java
@@ -0,0 +1,7 @@
+package de.qabel.core;
+
+
+public class EventNameConstants {
+	public static final String EVENT_CONTACT_ADDED = "contactAdded";
+	public static final String EVENT_CONTACT_REMOVED = "contactRemoved";
+}
--- src/main/java/de/qabel/core/config/ContactsActor.java
+++ src/main/java/de/qabel/core/config/ContactsActor.java
@@ -6,18 +6,26 @@ import java.util.ArrayList;
 import de.qabel.ackack.Actor;
 import de.qabel.ackack.MessageInfo;
 import de.qabel.ackack.Responsible;
+import de.qabel.ackack.event.EventEmitter;
+import de.qabel.core.EventNameConstants;
 
 /**
  * Actor which handles the access to contacts of Qabel.
+ * Emits EVENT_CONTACT_ADDED and EVENT_CONTACT_REMOVED.
  *
  */
 public class ContactsActor extends Actor {
 	private static ContactsActor defaultContactsActor = null;
-	private Contacts contacts;
+	private EventEmitter eventEmitter;
+	private final Contacts contacts;
 
-	static ContactsActor getDefault() {
+	/**
+	 * Get the default ContactActor. Uses the default EventEmitter.
+	 * @return default ContactActor
+	 */
+	public static ContactsActor getDefault() {
 		if (defaultContactsActor == null) {
-			defaultContactsActor = new ContactsActor(new Contacts());
+			defaultContactsActor = new ContactsActor(new Contacts(), EventEmitter.getDefault());
 		}
 		return defaultContactsActor;
 	}
@@ -26,8 +34,23 @@ public class ContactsActor extends Actor {
 	private static final String RETRIEVE_CONTACTS = "retrieveContacts";
 	private static final String REMOVE_CONTACTS = "removeContacts";
 
+	/**
+	 * Creates a new ContactActor with given contacts. Uses the default EventEmitter.
+	 * @param contacts Contacts to use in ContactActor
+	 */
 	public ContactsActor (Contacts contacts) {
 		this.contacts = contacts;
+		this.eventEmitter = EventEmitter.getDefault();
+	}
+
+	/**
+	 * Creates a new ContactActor with given contacts and EventEmitter.
+	 * @param contacts Contacts to use in ContactActor
+	 * @param eventEmitter EventEmitter to emit events from
+	 */
+	public ContactsActor(Contacts contacts, EventEmitter eventEmitter) {
+		this(contacts);
+		this.eventEmitter = eventEmitter;
 	}
 
 	/**
@@ -70,33 +93,36 @@ public class ContactsActor extends Actor {
 
 	@Override
 	protected void react(MessageInfo info, Object... data) {
-		switch(info.getType()) {
-		case WRITE_CONTACTS:
-			Contact contact;
-			for (int i = data.length-1; i>=0; i--) {
-				contact = (Contact) data[i];
-				this.contacts.replace(contact);
+		synchronized (contacts) {
+			switch (info.getType()) {
+				case WRITE_CONTACTS:
+					Contact contact;
+					for (int i = data.length - 1; i >= 0; i--) {
+						contact = (Contact) data[i];
+						this.contacts.replace(contact);
+						eventEmitter.emit(EventNameConstants.EVENT_CONTACT_ADDED, contact);
+					}
+					break;
+				case RETRIEVE_CONTACTS:
+					Contact[] contactsArray;
+					if(data.length > 0) {
+						ArrayList<Contact> contactsList = new ArrayList<Contact>();
+						for (int i = data.length - 1; i >= 0; i--) {
+							contactsList.add(this.contacts.getByKeyIdentifier((String) data[i]));
+						}
+						contactsArray = (Contact[]) contactsList.toArray(new Contact[0]);
+					} else {
+						contactsArray = this.contacts.getContacts().toArray(new Contact[0]);
+					}
+					info.response((Serializable[]) contactsArray);
+					break;
+				case REMOVE_CONTACTS:
+					for (int i = data.length - 1; i >= 0; i--) {
+						this.contacts.remove(data[i].toString());
+						eventEmitter.emit(EventNameConstants.EVENT_CONTACT_REMOVED, data[i].toString());
+					}
+					break;
 			}
-			break;
-		case RETRIEVE_CONTACTS:
-			Contact[] contactsArray;
-			if(data.length > 0) {
-				ArrayList<Contact> contactsList = new ArrayList<Contact>();
-				for(int i = data.length-1; i >= 0; i--){
-					contactsList.add(this.contacts.getByKeyIdentifier((String) data[i]));
-				}
-				contactsArray = (Contact[]) contactsList.toArray(new Contact[0]);
-			}
-			else {
-				contactsArray = this.contacts.getContacts().toArray(new Contact[0]);
-			}
-			info.response((Serializable[]) contactsArray);
-			break;
-		case REMOVE_CONTACTS:
-			for (int i = data.length-1; i>=0; i--) {
-				this.contacts.remove(data[i].toString());
-			}
-			break;
 		}
 		stop();
 	}
