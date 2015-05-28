43aba78e890177933db67fb2f34d223b776b631f
L-Henke <henke@praemandatum.de>
2015-05-28 16:49:16 +0200

    Added Javadoc for Persistable

--- src/main/java/de/qabel/core/config/Persistable.java
+++ src/main/java/de/qabel/core/config/Persistable.java
@@ -3,6 +3,10 @@ package de.qabel.core.config;
 import java.io.Serializable;
 import java.util.UUID;
 
+/**
+ * Persistable manages a unique persistence ID for objects that
+ * have to be persistable.
+ */
 public abstract class Persistable implements Serializable {
 
 	private UUID persistenceID;
