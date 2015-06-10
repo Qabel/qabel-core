ceb98f16e1c16206eca24788467f2f0edaeca260
Christian Burkert <burkert@praemandatum.de>
2015-06-10 17:26:21 +0200

    Changed AbstractModuleSettings scope to package private

--- src/main/java/de/qabel/core/config/AbstractModuleSettings.java
+++ src/main/java/de/qabel/core/config/AbstractModuleSettings.java
@@ -2,6 +2,6 @@ package de.qabel.core.config;
 
 import java.io.Serializable;
 
-public abstract class AbstractModuleSettings implements Serializable {
+abstract class AbstractModuleSettings implements Serializable {
 	private static final long serialVersionUID = -2650189278846561182L;
 }
