f7912ef5969fe1c63a156c91198f498757de8f85
Christian Burkert <burkert@praemandatum.de>
2015-06-10 17:31:27 +0200

    Made LocaleModuleSettings and SyncedModuleSettings abstract
    They should be instantiated by module-defined settings classes.

--- src/main/java/de/qabel/core/config/SyncedModuleSettings.java
+++ src/main/java/de/qabel/core/config/SyncedModuleSettings.java
@@ -1,5 +1,12 @@
 package de.qabel.core.config;
 
-public class SyncedModuleSettings extends AbstractModuleSettings {
+/**
+ * Module settings that should be synchronized over multiple Qabel installations
+ * should be grouped in a module-defined settings class that inherits from this
+ * class.
+ * 
+ * @see LocaleModuleSettings
+ */
+public abstract class SyncedModuleSettings extends AbstractModuleSettings {
 	private static final long serialVersionUID = 7632514690237416721L;
 }
