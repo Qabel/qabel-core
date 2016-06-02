4a4984901a6df3b984ea8c7db1432cddb496af3e
julianseeger <seeger.julian@gmail.com>
2016-06-02 13:59:18 +0200

    #502 test that dependency to core and libcurve works

--- box/src/test/java/de/qabel/box/PlaceholderTest.java
+++ box/src/test/java/de/qabel/box/PlaceholderTest.java
@@ -1,12 +1,25 @@
 package de.qabel.box;
 
+import de.qabel.core.crypto.CryptoUtils;
 import org.junit.Test;
+import org.spongycastle.crypto.params.KeyParameter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PlaceholderTest {
     @Test
     public void compileCheckTest() {
         assertTrue(new Placeholder() instanceof Placeholder);
+    }
+
+    @Test
+    public void dependencyTest() throws Exception {
+        CryptoUtils utils = new CryptoUtils();
+        String message = "testmessage";
+        KeyParameter key = utils.generateSymmetricKey();
+        byte[] encryptedMessage = utils.encrypt(key, "?".getBytes(), message.getBytes(), "?".getBytes());
+        String decryptedMessage = new String(utils.decrypt(key, "?".getBytes(), encryptedMessage, "?".getBytes()));
+        assertEquals(decryptedMessage, message);
     }
 }
