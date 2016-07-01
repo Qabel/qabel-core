d2008f0fb66aabf61a47d34360376829af6a8d27
JoGir <girschik@qabel.de>
2016-07-01 16:00:10 +0200

    moved contacts formats to ContactExchangeFormats.kt

--- core/src/main/java/de/qabel/core/config/ContactExportImport.java
+++ core/src/main/java/de/qabel/core/config/ContactExportImport.java
@@ -1,60 +1,24 @@
 package de.qabel.core.config;
 
-import de.qabel.core.crypto.QblECPublicKey;
-import de.qabel.core.drop.DropURL;
+import de.qabel.core.contacts.ContactExchangeFormats;
 import de.qabel.core.exceptions.QblDropInvalidURL;
-import org.json.JSONArray;
+import de.qabel.core.exceptions.QblInvalidFormatException;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.spongycastle.util.encoders.Hex;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class ContactExportImport {
 
-    private static final String KEY_ALIAS = "alias";
-    private static final String KEY_EMAIL = "email";
-    private static final String KEY_PHONE = "phone";
-    private static final String KEY_PUBLIC_KEY = "public_key";
-    private static final String KEY_DROP_URLS = "drop_urls";
-    private static final String KEY_CONTACTS = "contacts";
+    private static final ContactExchangeFormats exchangeFormats = new ContactExchangeFormats();
 
     public static String exportContacts(Contacts contacts) throws JSONException {
-
-        JSONObject jsonObject = new JSONObject();
-        JSONArray jsonContacts = new JSONArray();
-        for (Contact contact : contacts.getContacts()) {
-            jsonContacts.put(getJSONfromContact(contact));
-        }
-        jsonObject.put(KEY_CONTACTS, jsonContacts);
-        return jsonObject.toString();
+        return exchangeFormats.exportToContactsJSON(contacts.getContacts());
     }
 
     public static String exportContact(Contact contact) {
-
-        return getJSONfromContact(contact).toString();
-    }
-
-    private static JSONObject getJSONfromContact(Contact contact) {
-
-        JSONObject jsonObject = new JSONObject();
-        JSONArray jsonDropUrls = new JSONArray();
-        try {
-            jsonObject.put(KEY_ALIAS, contact.getAlias());
-            jsonObject.put(KEY_EMAIL, contact.getEmail());
-            jsonObject.put(KEY_PHONE, contact.getPhone());
-            jsonObject.put(KEY_PUBLIC_KEY, contact.getKeyIdentifier());
-            for (DropURL dropURL : contact.getDropUrls()) {
-                jsonDropUrls.put(dropURL);
-            }
-            jsonObject.put(KEY_DROP_URLS, jsonDropUrls);
-        } catch (JSONException e) {
-            // Shouldn't be possible to trigger this exception
-            throw new RuntimeException("Cannot build JSONObject", e);
-        }
-        return jsonObject;
+        return exchangeFormats.exportToContactsJSON(Collections.singleton(contact));
     }
 
     /**
@@ -64,24 +28,7 @@ public class ContactExportImport {
      * @return {@link Contact} information as JSON string
      */
     public static String exportIdentityAsContact(Identity identity) {
-
-        JSONObject jsonObject = new JSONObject();
-        JSONArray jsonDropUrls = new JSONArray();
-        try {
-            jsonObject.put(KEY_ALIAS, identity.getAlias());
-            jsonObject.put(KEY_EMAIL, identity.getEmail());
-            jsonObject.put(KEY_PHONE, identity.getPhone());
-            jsonObject.put(KEY_PUBLIC_KEY, identity.getKeyIdentifier());
-            for (DropURL dropURL : identity.getDropUrls()) {
-                jsonDropUrls.put(dropURL);
-            }
-            jsonObject.put(KEY_DROP_URLS, jsonDropUrls);
-        } catch (JSONException e) {
-            // Shouldn't be possible to trigger this exception
-            throw new RuntimeException("Cannot build JSONObject", e);
-        }
-
-        return jsonObject.toString();
+        return exchangeFormats.exportToContactsJSON(identity);
     }
 
     /**
@@ -91,8 +38,11 @@ public class ContactExportImport {
      * @return {@link Contact} parsed from JSON string
      */
     public static Contact parseContactForIdentity(String json) throws JSONException, URISyntaxException, QblDropInvalidURL {
-
-        return parseContactFromJSON(json);
+        try {
+            return exchangeFormats.importFromContactsJSON(json).get(0);
+        }catch (IndexOutOfBoundsException | QblInvalidFormatException e){
+            throw new JSONException(e);
+        }
     }
 
     /**
@@ -103,36 +53,16 @@ public class ContactExportImport {
      * @return {@link Contacts} parsed from JSON string
      */
     public static Contacts parseContactsForIdentity(Identity identity, String json) throws JSONException, URISyntaxException, QblDropInvalidURL {
-
         Contacts contacts = new Contacts(identity);
-        JSONObject jsonObject = new JSONObject(json);
-        JSONArray jsonContacts = jsonObject.getJSONArray(KEY_CONTACTS);
-        for (int i = 0; i < jsonContacts.length(); i++) {
-            JSONObject o = (JSONObject) jsonContacts.get(i);
-            contacts.put(parseContactFromJSON(o.toString()));
-        }
-        return contacts;
-    }
+        try {
 
-    private static Contact parseContactFromJSON(String json) throws JSONException, URISyntaxException, QblDropInvalidURL {
-
-        JSONObject jsonObject = new JSONObject(json);
-
-        Collection<DropURL> dropURLs = new ArrayList<>();
-        String alias = jsonObject.getString(KEY_ALIAS);
-        JSONArray jsonDropURLS = jsonObject.getJSONArray(KEY_DROP_URLS);
-        for (int i = 0; i < jsonDropURLS.length(); i++) {
-            dropURLs.add(new DropURL(jsonDropURLS.getString(i)));
+            List<Contact> contactList = exchangeFormats.importFromContactsJSON(json);
+            for(Contact contact : contactList){
+                contacts.put(contact);
+            }
+            return contacts;
+        } catch (QblInvalidFormatException e) {
+            throw new JSONException(e);
         }
-        String keyIdentifier = jsonObject.getString(KEY_PUBLIC_KEY);
-
-        Contact contact = new Contact(alias, dropURLs, new QblECPublicKey(Hex.decode(keyIdentifier)));
-        if (jsonObject.has(KEY_EMAIL)) {
-            contact.setEmail(jsonObject.getString(KEY_EMAIL));
-        }
-        if (jsonObject.has(KEY_PHONE)) {
-            contact.setPhone(jsonObject.getString(KEY_PHONE));
-        }
-        return contact;
     }
 }
---
+++ core/src/test/java/de/qabel/core/contacts/ContactExchangeFormatsTest.kt
@@ -0,0 +1,86 @@
+package de.qabel.core.contacts
+
+import de.qabel.core.config.Contact
+import de.qabel.core.config.Contacts
+import de.qabel.core.config.Identity
+import de.qabel.core.crypto.QblECKeyPair
+import de.qabel.core.drop.DropURL
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is
+import org.junit.Before
+import org.junit.Test
+
+class ContactExchangeFormatsTest {
+
+    companion object {
+        private val DROP_URL_1 = "http://localhost:6000/1234567890123456789012345678901234567891234"
+        private val DROP_URL_2 = "http://localhost:6000/0000000000000000000000000000000000000000000"
+    }
+
+    internal lateinit var identity: Identity
+    internal lateinit var contact1: Contact
+    internal lateinit var contact2: Contact
+    internal lateinit var contacts: Contacts
+    internal lateinit var qblECKeyPair: QblECKeyPair
+
+    internal lateinit var contactExchangeFormats: ContactExchangeFormats
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        contactExchangeFormats = ContactExchangeFormats();
+
+        val dropURLs = listOf(DropURL(DROP_URL_1), DropURL(DROP_URL_2));
+
+        qblECKeyPair = QblECKeyPair()
+        identity = Identity("Identity", dropURLs, qblECKeyPair)
+        identity.email = "test@example.com"
+        identity.phone = "+491111111"
+
+        val contact1KeyPair = QblECKeyPair()
+        contact1 = Contact("Contact1", dropURLs, contact1KeyPair.pub)
+
+        val contact2KeyPair = QblECKeyPair()
+        contact2 = Contact("Contact2", dropURLs, contact2KeyPair.pub)
+
+        contacts = Contacts(identity)
+        contacts.put(contact1)
+        contacts.put(contact2)
+    }
+
+    private fun assertContactEquals(contact1: Contact, contact2: Contact) {
+        assertThat(contact1.alias, Is.`is`(contact2.alias))
+        assertThat(contact1.dropUrls, Is.`is`(contact2.dropUrls))
+        assertThat(contact1.ecPublicKey.readableKeyIdentifier, Is.`is`(contact2.ecPublicKey.readableKeyIdentifier))
+        assertThat(contact1.phone, Is.`is`(contact2.phone))
+        assertThat(contact1.email, Is.`is`(contact2.email))
+    }
+
+    @Test
+    fun testImportExportContactJSON() {
+        val contactJSON = contactExchangeFormats.exportToContactsJSON(setOf(contact1));
+        val parsedContacts = contactExchangeFormats.importFromContactsJSON(contactJSON);
+        assertThat(1, Is.`is`(parsedContacts.size));
+        assertContactEquals(contact1, parsedContacts.first());
+    }
+
+    @Test
+    fun testImportExportContactsJSON() {
+        val contactsJSON = contactExchangeFormats.exportToContactsJSON(contacts.contacts);
+        val parsedContacts = contactExchangeFormats.importFromContactsJSON(contactsJSON);
+        assertThat(contacts.contacts.size, Is.`is`(parsedContacts.size));
+        for (contact in parsedContacts) {
+            assertContactEquals(contacts.getByKeyIdentifier(contact.keyIdentifier),
+                contact);
+        }
+    }
+
+    @Test
+    fun testImportExportContactString() {
+        val contactString = contactExchangeFormats.exportToContactString(contact1);
+        val parsedContact = contactExchangeFormats.importFromContactString(contactString);
+        assertContactEquals(contact1, parsedContact);
+    }
+
+}
+
