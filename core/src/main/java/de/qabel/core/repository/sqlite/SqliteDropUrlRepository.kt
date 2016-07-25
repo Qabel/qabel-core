093bc47240f68ea2d46eabd9d3ba8be605d4246f
JoGir <girschik@qabel.de>
2016-07-25 14:17:56 +0200

    move dropUrlRepo to kotlin and add tests

---
+++ core/src/main/java/de/qabel/core/repository/sqlite/SqliteDropUrlRepository.kt
@@ -0,0 +1,93 @@
+package de.qabel.core.repository.sqlite
+
+import de.qabel.core.StringUtils
+import de.qabel.core.config.Contact
+import de.qabel.core.config.Entity
+import de.qabel.core.drop.DropURL
+import de.qabel.core.exceptions.QblDropInvalidURL
+import de.qabel.core.extensions.use
+import de.qabel.core.repository.exception.PersistenceException
+import de.qabel.core.util.DefaultHashMap
+import java.net.URISyntaxException
+import java.sql.SQLException
+import java.util.*
+
+class SqliteDropUrlRepository(database: ClientDatabase, hydrator: Hydrator<DropURL>) : AbstractSqliteRepository<DropURL>(database, hydrator, SqliteDropUrlRepository.TABLE_NAME) {
+
+    companion object {
+        val TABLE_NAME = "drop_url"
+    }
+
+    @Deprecated("@todo hide contactId and use findAll(Contact contact) as soon as Identity extends Contact")
+    @Throws(PersistenceException::class)
+    fun findAll(contactId: Int): Collection<DropURL> {
+        return findAll("contact_id=?", contactId)
+    }
+
+    @Throws(PersistenceException::class)
+    fun findAll(contact: Contact): Collection<DropURL> {
+        return findAll(contact.id)
+    }
+
+    @Throws(SQLException::class)
+    fun delete(contact: Contact) {
+        val contactId = contact.id
+        delete(contactId)
+    }
+
+
+    @Deprecated("@todo hide contactId and use delete(Contact contact) as soon as Identity extends Contact")
+    @Throws(SQLException::class)
+    fun delete(contactId: Int) {
+        database.prepare(
+            "DELETE FROM $TABLE_NAME WHERE contact_id = ?").use { dropDrops ->
+            dropDrops.setInt(1, contactId)
+            dropDrops.execute()
+        }
+    }
+
+    @Throws(SQLException::class)
+    fun store(contact: Contact) {
+        val contactId = contact.id
+        store(contact, contactId)
+    }
+
+    @Deprecated("@todo hide contactId and use store(Contact contact) as soon as Identity extends Contact")
+    @Throws(SQLException::class)
+    fun store(contact: Entity, contactId: Int) {
+        database.prepare(
+            "INSERT INTO $TABLE_NAME (contact_id, url) VALUES (?, ?)").use { dropStatement ->
+            for (url in contact.dropUrls) {
+                dropStatement.setInt(1, contactId)
+                dropStatement.setString(2, url.toString())
+                dropStatement.execute()
+            }
+        }
+    }
+
+    @Throws(PersistenceException::class)
+    fun findDropUrls(contactIds: List<Int>): Map<Int, List<DropURL>> {
+        try {
+            val contactDropUrlMap = DefaultHashMap<Int, MutableList<DropURL>>({ LinkedList<DropURL>() })
+            database.prepare(
+                "SELECT contact_id, url " +
+                    "FROM " + TABLE_NAME + " urls " +
+                    "WHERE contact_id IN (" + StringUtils.join(",", contactIds) + ")").use { statement ->
+                statement.executeQuery().use({ results ->
+                    while (results.next()) {
+                        val id = results.getInt(1)
+                        val dropUrl = DropURL(results.getString(2))
+                        contactDropUrlMap.getOrDefault(id).add(dropUrl)
+                    }
+                })
+            }
+            return contactDropUrlMap
+        } catch (e: SQLException) {
+            throw PersistenceException("Error loading dropUrls for contacts", e)
+        } catch (e: QblDropInvalidURL) {
+            throw PersistenceException("Error loading dropUrls for contacts", e)
+        } catch (e: URISyntaxException) {
+            throw PersistenceException("Error loading dropUrls for contacts", e)
+        }
+    }
+}
---
+++ core/src/test/java/de/qabel/core/repository/SqliteDropUrlRepositoryTest.kt
@@ -0,0 +1,88 @@
+package de.qabel.core.repository
+
+import de.qabel.core.config.Contact
+import de.qabel.core.config.factory.DropUrlGenerator
+import de.qabel.core.config.factory.IdentityBuilder
+import de.qabel.core.crypto.QblECPublicKey
+import de.qabel.core.drop.DropURL
+import de.qabel.core.repository.sqlite.*
+import de.qabel.core.repository.sqlite.hydrator.DropURLHydrator
+import org.hamcrest.Matchers.*
+import java.util.*
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+
+class SqliteDropUrlRepositoryTest : AbstractSqliteRepositoryTest<SqliteDropUrlRepository>() {
+
+    lateinit var contactRepo: ContactRepository
+
+    val dropGenerator = DropUrlGenerator("http://localhost")
+    val identityA = IdentityBuilder(dropGenerator).withAlias("identityA").build()
+    val contactA = Contact("contactA", LinkedList<DropURL>(), QblECPublicKey("test13".toByteArray()));
+    val contactB = Contact("contactB", LinkedList<DropURL>(), QblECPublicKey("test24".toByteArray()));
+
+    override fun createRepo(clientDatabase: ClientDatabase, em: EntityManager): SqliteDropUrlRepository? {
+        val identityRepo = SqliteIdentityRepository(clientDatabase, em)
+        contactRepo = SqliteContactRepository(clientDatabase, em, identityRepo)
+        identityRepo.save(identityA)
+        contactRepo.save(contactA, identityA)
+        contactRepo.save(contactB, identityA)
+        return SqliteDropUrlRepository(clientDatabase, DropURLHydrator())
+    }
+
+    @Test
+    fun testSave() {
+        val dropUrl = dropGenerator.generateUrl()
+        contactA.addDrop(dropUrl)
+        repo.store(contactA)
+
+        assertThat(repo.findAll(contactA), contains(dropUrl))
+    }
+
+    @Test
+    fun testDelete() {
+        val dropUrl = dropGenerator.generateUrl()
+        contactA.addDrop(dropUrl)
+        repo.store(contactA)
+        assertThat(repo.findAll(contactA), contains(dropUrl))
+
+        repo.delete(contactA)
+
+        assertThat(repo.findAll(contactA), hasSize(0))
+    }
+
+    @Test
+    fun testFindAll() {
+        val dropUrls = listOf(dropGenerator.generateUrl(), dropGenerator.generateUrl(), dropGenerator.generateUrl())
+        dropUrls.forEach { contactA.addDrop(it) }
+        repo.store(contactA)
+
+        assertThat(repo.findAll(contactA), containsInAnyOrder(*dropUrls.toTypedArray()))
+    }
+
+    @Test
+    fun testFindByContactIds() {
+        val dropUrlsA = listOf(dropGenerator.generateUrl(), dropGenerator.generateUrl())
+        dropUrlsA.forEach { contactA.addDrop(it) }
+        repo.store(contactA)
+
+        val dropUrlB = dropGenerator.generateUrl()
+        contactB.addDrop(dropUrlB)
+        repo.store(contactB)
+
+        val result = repo.findDropUrls(listOf(contactA.id, contactB.id))
+        assertThat(result.keys, hasSize(2))
+        assertThat(result.keys, containsInAnyOrder(contactA.id, contactB.id))
+
+        val resultA = result[contactA.id]
+        assertThat(resultA, hasSize(2))
+        assertThat(resultA, containsInAnyOrder(*dropUrlsA.toTypedArray()))
+
+        val resultB = result[contactB.id]
+        assertThat(resultB, hasSize(1))
+        assertThat(resultB, contains(dropUrlB))
+    }
+
+
+}
