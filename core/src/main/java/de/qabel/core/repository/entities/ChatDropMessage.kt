ffad19839e00a23a2953c479c1bc72fe3cac9756
JoGir <girschik@qabel.de>
2016-07-26 21:06:27 +0200

    add tmp RuntimeException for JsonDecoding

--- core/src/main/java/de/qabel/core/repository/entities/ChatDropMessage.kt
+++ core/src/main/java/de/qabel/core/repository/entities/ChatDropMessage.kt
@@ -43,9 +43,9 @@ data class ChatDropMessage(val contactId: Int,
             fun decode(messageType: MessageType, content: String): MessagePayload {
                 val gson = Gson()
                 if (messageType == MessageType.SHARE_NOTIFICATION) {
-                    return gson.fromJson(content, ShareMessage::class.java)
+                    return gson.fromJson(content, ShareMessage::class.java) ?: throw RuntimeException("Error decoding json " + content)
                 } else if (messageType == MessageType.BOX_MESSAGE) {
-                    return gson.fromJson(content, TextMessage::class.java)
+                    return gson.fromJson(content, TextMessage::class.java) ?: throw RuntimeException("Error decoding json " + content)
                 }
                 throw RuntimeException("Unknown MessageType")
             }
