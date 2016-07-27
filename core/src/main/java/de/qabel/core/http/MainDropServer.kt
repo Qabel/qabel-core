7cde20727e5a25039de192ba0fbaca3e6714c424
JoGir <girschik@qabel.de>
2016-07-27 16:34:10 +0200

    remove apache http dependencies

--- core/src/main/java/de/qabel/core/http/MainDropServer.kt
+++ core/src/main/java/de/qabel/core/http/MainDropServer.kt
@@ -2,72 +2,82 @@ package de.qabel.core.http
 
 import de.qabel.core.exceptions.QblDropInvalidMessageSizeException
 import de.qabel.core.exceptions.QblDropInvalidURL
-import de.qabel.core.http.DropServerHttp.*
+import de.qabel.core.http.DropServerHttp.QblHeaders
+import de.qabel.core.http.DropServerHttp.QblStatusCodes
 import org.apache.commons.io.IOUtils
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.entity.ByteArrayEntity
-import org.apache.http.entity.ContentType
-import org.apache.http.impl.client.HttpClients
 import org.apache.james.mime4j.stream.EntityState
 import org.apache.james.mime4j.stream.MimeTokenStream
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
 import java.net.URI
 
 class MainDropServer : DropServerHttp {
 
-    override fun sendBytes(uri: URI, messageBytes: ByteArray) {
-        HttpClients.createDefault().use {
-            val request = HttpPost(uri);
-            request.addHeader(QblHeaders.AUTHORIZATION, DropServerHttp.DEFAULT_AUTH_TOKEN)
-            request.entity = ByteArrayEntity(messageBytes).apply { setContentType(ContentType.APPLICATION_OCTET_STREAM.toString()) }
-            it.execute(request).use {
-                when (it.statusLine.statusCode) {
-                    QblStatusCodes.OK -> Unit
-                    QblStatusCodes.INVALID -> throw QblDropInvalidURL()
-                    QblStatusCodes.INVALID_SIZE -> throw QblDropInvalidMessageSizeException()
-                    else -> throw RuntimeException("Received unknown statusCode $it.statusLine.statusCode")
-                }
-            }
-        }
+    companion object {
+        const val DROP_CONTENT_TYPE_KEY = "Content-Type"
+        const val DROP_CONTENT_TYPE = "application/octet-stream"
     }
 
-    override fun receiveMessageBytes(uri: URI, eTag: String): Triple<Int, String, Collection<ByteArray>> {
-        HttpClients.createDefault().use {
-            it.execute(HttpGet(uri).apply {
-                if (!eTag.isEmpty()) {
-                    addHeader(QblHeaders.X_QABEL_LATEST, eTag)
-                }
-            }).use { response ->
-                val statusCode = response.statusLine.statusCode
-                val messages: List<ByteArray> = when (statusCode) {
-                    QblStatusCodes.OK -> {
-                        response.entity.content.use {
-                            val stream = MimeTokenStream()
-                            stream.parseHeadless(it, response.entity.contentType.value)
-                            val messages = mutableListOf<ByteArray>()
-                            var state = stream.state;
-                            while (state != EntityState.T_END_OF_STREAM) {
-                                if (state == EntityState.T_BODY) {
-                                    val bytes = IOUtils.toByteArray(stream.inputStream)
-                                    messages.add(bytes)
-                                }
-                                state = stream.next()
-                            }
-                            messages
-                        }
-                    }
-                    QblStatusCodes.NOT_MODIFIED -> emptyList()
-                    QblStatusCodes.EMPTY_DROP -> emptyList()
-                    QblStatusCodes.INVALID -> throw QblDropInvalidURL()
-                    else -> throw RuntimeException("Received unknown statusCode")
-                }
-                var responseETag = ""
-                if (response.containsHeader(QblHeaders.X_QABEL_LATEST)) {
-                    responseETag = response.getFirstHeader(QblHeaders.X_QABEL_LATEST).value
-                }
-                return Triple(statusCode, responseETag, messages)
+    override fun sendBytes(uri: URI, messageBytes: ByteArray) =
+        connect(uri).use {
+            it.doOutput = true // indicates POST method
+            it.setRequestProperty(DROP_CONTENT_TYPE_KEY, DROP_CONTENT_TYPE)
+            it.setRequestProperty(QblHeaders.AUTHORIZATION, DropServerHttp.DEFAULT_AUTH_TOKEN)
+
+            DataOutputStream(it.outputStream).use {
+                it.write(messageBytes)
             }
+            when (it.responseCode) {
+                QblStatusCodes.OK -> Unit
+                QblStatusCodes.INVALID -> throw QblDropInvalidURL()
+                QblStatusCodes.INVALID_SIZE -> throw QblDropInvalidMessageSizeException()
+                else -> throw RuntimeException("Received unknown statusCode $it.statusLine.statusCode")
+            }
+        }
+
+    override fun receiveMessageBytes(uri: URI, eTag: String): Triple<Int, String, Collection<ByteArray>> =
+        connect(uri).use {
+            if (!eTag.isEmpty()) {
+                it.addRequestProperty(QblHeaders.X_QABEL_LATEST, eTag)
+            }
+
+            val statusCode = it.responseCode
+            val messages: List<ByteArray> = when (statusCode) {
+                QblStatusCodes.OK -> {
+                    it.inputStream.use { inputStream ->
+                        val stream = MimeTokenStream()
+                        stream.parseHeadless(inputStream, it.contentType)
+                        val messages = mutableListOf<ByteArray>()
+                        var state = stream.state;
+                        while (state != EntityState.T_END_OF_STREAM) {
+                            if (state == EntityState.T_BODY) {
+                                val bytes = IOUtils.toByteArray(stream.inputStream)
+                                messages.add(bytes)
+                            }
+                            state = stream.next()
+                        }
+                        messages
+                    }
+                }
+                QblStatusCodes.NOT_MODIFIED -> emptyList()
+                QblStatusCodes.EMPTY_DROP -> emptyList()
+                QblStatusCodes.INVALID -> throw QblDropInvalidURL()
+                else -> throw RuntimeException("Received unknown statusCode")
+            }
+            var responseETag = ""
+            if (!it.getHeaderField(QblHeaders.X_QABEL_LATEST).isEmpty()) {
+                responseETag = it.getHeaderField(QblHeaders.X_QABEL_LATEST)
+            }
+            Triple(statusCode, responseETag, messages)
+        }
+
+    private fun connect(url: URI) = url.toURL().openConnection() as HttpURLConnection
+
+    fun <X> HttpURLConnection.use(block: (HttpURLConnection) -> X): X {
+        try {
+            return block(this)
+        } finally {
+            disconnect()
         }
     }
 }
