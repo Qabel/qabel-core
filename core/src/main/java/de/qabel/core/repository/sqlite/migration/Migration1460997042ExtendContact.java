c968c7ea39104677e7ac74447f82a091c88b7004
JoGir <girschik@qabel.de>
2016-08-24 13:39:54 +0200

    fix migration

--- core/src/main/java/de/qabel/core/repository/sqlite/migration/Migration1460997042ExtendContact.java
+++ core/src/main/java/de/qabel/core/repository/sqlite/migration/Migration1460997042ExtendContact.java
@@ -20,7 +20,7 @@ public class Migration1460997042ExtendContact extends AbstractMigration {
         execute("ALTER TABLE contact ADD COLUMN status INTEGER NOT NULL DEFAULT 1");
         execute("ALTER TABLE contact ADD COLUMN ignored BOOLEAN NOT NULL DEFAULT FALSE");
         //Default STATUS=NORMAL and not ignored
-        execute("UPDATE contact set nickname=alias, ignored=false, status=1");
+        execute("UPDATE contact set nickname=alias, ignored=0, status=1");
     }
 
     @Override
