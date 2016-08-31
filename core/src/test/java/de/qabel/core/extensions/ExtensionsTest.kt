a90624239518c0604d08e102b12de52dbcc8fde9
JoGir <girschik@qabel.de>
2016-08-31 19:39:51 +0200

    fix initials, again

--- core/src/test/java/de/qabel/core/extensions/ExtensionsTest.kt
+++ core/src/test/java/de/qabel/core/extensions/ExtensionsTest.kt
@@ -1,18 +1,33 @@
 package de.qabel.core.extensions
 
+import de.qabel.core.config.Contact
+import de.qabel.core.ui.initials
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.*
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Test
 
 
 class ExtensionsTest() {
 
-    data class Test(var text: String) {}
+    data class TestData(var text: String) {}
 
-    @org.junit.Test
+    @Test
     fun testLetApply() {
         val text = "Banane"
-        val textObj = Test("Kirsche").letApply {
+        val textObj = TestData("Kirsche").letApply {
             it.text = text
         }
         assertEquals("Banane", textObj.text)
     }
+
+    @Test
+    fun testInitials() {
+        val initials = Contact("Eine kleiner Test", emptyList(), null).initials()
+        assertThat(initials.length, equalTo(2))
+        val zwo = Contact("E", emptyList(), null).initials()
+        assertThat(zwo.length, equalTo(1))
+    }
+
 }
