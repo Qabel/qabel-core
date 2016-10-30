7ca3c512eb62da25606b9c8e129c7919c7636481
julianseeger <seeger.julian@gmail.com>
2016-10-30 21:01:58 +0100

    use rx ofType instead of an instanceof filter

--- core/src/main/java/de/qabel/core/event/EventDispatcher.kt
+++ core/src/main/java/de/qabel/core/event/EventDispatcher.kt
@@ -5,7 +5,7 @@ import rx.Observable
 interface EventSource {
     fun events(): Observable<Event>
     fun <T : Event> events(type: Class<T>): Observable<T>
-        = events().filter { type.isInstance(it) }.map { type.cast(it) }
+        = events().ofType(type).map { type.cast(it) }
 }
 interface EventSink {
     fun push(event: Event)
